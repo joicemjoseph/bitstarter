94148ca9f0b8ae76609e7678aefcc9d406c8eeea
Joice Joseph <joicefootball@gmail.com>
2013-07-05 13:22:26 +0530

    Added index.html

--- web.js
+++ web.js
@@ -1,10 +1,26 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+ var fs = require("fs");
+ var fileToRead = "index.html";
+ fs.readFile(fileToRead, function (err, data) {
+  if (err) throw err;
+  console.log(data);
+	app.get('/', function(request, response) {
+  	response.send(data);
+	});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+
+
 });
+
+
+
+
+
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
