627b76dd36fcb247d12ac9072cd398726794948e
Joice Joseph <joicefootball@gmail.com>
2013-07-05 13:46:51 +0530

    edited

--- web.js
+++ web.js
@@ -1,3 +1,16 @@
-var express = require('express');
-var app = express.createServer(express.logger());
-app.get("/", function(request, response) { res.send(index.html); });
+var fs = require("fs");
+var fileName = "foo.txt";
+fs.exists(fileName, function(exists) {
+  if (exists) {
+    fs.stat(fileName, function(error, stats) {
+      fs.open(fileName, "r", function(error, fd) {
+        var buffer = new Buffer(stats.size);
+        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
+          var data = buffer.toString("utf8", 0, buffer.length);
+          console.log(data);
+          fs.close(fd);
+        });
+      });
+    });
+  }
+});
