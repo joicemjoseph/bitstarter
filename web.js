d8066eb561cfcb3e9a69c4cf90243115efb58b4f
Joice Joseph <joicefootball@gmail.com>
2013-07-05 13:51:22 +0530

    edited- major

--- web.js
+++ web.js
@@ -1,5 +1,7 @@
+var express = require('express');
+var app = express.createServer(express.logger());
 var fs = require("fs");
-var fileName = "foo.txt";
+var fileName = "index.html";
 fs.exists(fileName, function(exists) {
   if (exists) {
     fs.stat(fileName, function(error, stats) {
@@ -7,10 +9,18 @@ fs.exists(fileName, function(exists) {
         var buffer = new Buffer(stats.size);
         fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
           var data = buffer.toString("utf8", 0, buffer.length);
+		app.get('/', function(request, response) {
+ 		 response.send(data);
+		});
           console.log(data);
           fs.close(fd);
         });
       });
     });
   }
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
 });
