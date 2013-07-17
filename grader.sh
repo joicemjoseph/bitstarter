d8bceadb4d03862f17ff34ca3c6c15f093cf439c
Joice Joseph <joicefootball@gmail.com>
2013-07-17 09:10:56 +0100

    CSS, js and images added

---
+++ grader.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+HTMLROOT="http://gentle-savannah-3580.herokuapp.com"
+
+#./grader.js --checks checks.json --file index.html
+#./grader.js --checks checks.json --url index.html $HTMLROOT
+./grader.js --checks checks.json --url $HTMLROOT
